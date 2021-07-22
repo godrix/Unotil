0c9dc895e9858a878576b2696dbd09ebf1aba548
Carlos Godri <cggodri@gmail.com>
2021-07-21 21:06:34 -0300

    add function random color

--- src/screen/RandomColor/index.tsx
+++ src/screen/RandomColor/index.tsx
@@ -1,9 +1,37 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, {useState} from 'react';
 
-//import { Container } from './styles';
+import { Container, BoxColor, Line } from './styles';
 
 
 export function RandomColor() {
-  return <View />;
+  const [ramdomColor, setRandomColor] = useState(1);
+  const [init, setInit] = useState(false);
+
+  function nextColor(){
+    const ramdomNumber = Math.floor(Math.random() * 4) + 1;
+    setRandomColor(ramdomNumber);
+  }
+
+  function randomNumberIn5Secconds(){
+    setInit(true)
+    if(init){
+      setInit(false)
+      const interval = setInterval(nextColor, 100);
+      setTimeout(()=>{
+        clearInterval(interval);
+      }, 3000)
+    }
+   
+  }
+
+  return <Container onPress={randomNumberIn5Secconds}>
+    <Line>
+    <BoxColor color="red" active={ramdomColor === 1} />
+    <BoxColor color="blue" active={ramdomColor === 2} />
+    </Line>
+   <Line>
+   <BoxColor color="yellow" active={ramdomColor === 3}/>
+    <BoxColor color="green" active={ramdomColor === 4}/>
+   </Line>
+  </Container>;
 }
--- src/screen/RandomColor/styles.ts
+++ src/screen/RandomColor/styles.ts
@@ -1,5 +1,24 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
+export const Container = styled.TouchableOpacity.attrs({
+  activeOpacity: 0.7,
+})`
+  flex:1;
+`;
+
+interface BoxColorProps{
+  color:'red'|'blue'|'green'|'yellow';
+  active?:boolean;
+}
+
+export const Line = styled.View`
+  flex:2;
+  flex-direction: row;
+`;
+
+export const BoxColor = styled.View<BoxColorProps>`
+  background: ${({color})=>color};
+  ${({active})=>active && 'border-color: white; border-width:5px'}
   
+  flex:2;
 `;
